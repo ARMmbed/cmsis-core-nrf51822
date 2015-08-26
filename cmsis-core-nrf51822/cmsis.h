480273b4f796a845c7d19b3bc932c290cca64288
Rohit Grover <rohit.grover@arm.com>
2015-08-26 07:40:59 +0100

    update copyright and license to ARM/Apache on cmsis.h

--- cmsis-core-nrf51822/cmsis.h
+++ cmsis-core-nrf51822/cmsis.h
@@ -1,5 +1,6 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2006-2013 ARM Limited
+/*
+ * PackageLicenseDeclared: Apache-2.0
+ * Copyright (c) 2015 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
